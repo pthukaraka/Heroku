b1a01836ff6913d66851a73dded119a4e8740b81
pthukaraka <65617857+pthukaraka@users.noreply.github.com>
2020-05-20 14:49:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ def webhook():
         int_features=json.loads(res)
         print(int_features)
         final_features=pd.DataFrame({'age':[int(int_features['age']['amount'])],'sex':[int(int_features['sex'])],'cp':[int(int_features['cp'])],'trestbps':[int(int_features['trestbps'])],'chol':[int(int_features['chol'])],'fbs':[int(int_features['fbs'])],'restecg':[int(int_features['restecg'])],'thalach':[int(int_features['thalach'])],'exang':[int(int_features['exang'])],'oldpeak':[float(int_features['oldpeak'])],'slope':[float(int_features['slope'])],'ca':[int(int_features['ca'])],'thal':[int(int_features['thal'])]})
-        prediction=model.predict(final_features)
-        pred=((prediction[0][1]*0.842)*100)
-        r=get_data(prediction)
+        prediction=model.predict_proba(final_features)
+        print(prediction)
+        pred=(prediction[0][0]*0.842)
+        r=get_data(pred)
         print(r)
         r=json.dumps(r)
         result = make_response(r)
@@ -29,16 +30,14 @@ def webhook():
         return result
      
  
-def get_data(prediction):
-   output=prediction[0][1]*100
-   if output >=50:
-        pred='You have '+ str(output)+'%'+' possibility of having heart disease.Please take immediate action to cure your self.'
-   elif 30<=output<=50:
-        pred='You have '+ str(output)+'%'+' possibility of having heart disease.Please take any actions and precautions to not to get a heart disease.'
-   elif 0<output<=30:
-        pred='You have '+ str(output)+'%'+' possibility of having heart disease.Anyway you have to be careful.'
-   elif 0==output:
-        pred='You have '+ str(output)+'%'+' possibility of having heart disease.You are safe'
+def get_data(pred):
+   
+   if (pred<0.5):
+        pred='You have '+ str((1-pred)*100)+'%'+' possibility of having heart disease.Please take immediate action to cure your self.'
+   elif (0.5<=pred<0.75):
+        pred='You have '+ str((1-pred)*100)+'%'+' possibility of having heart disease.You have to be careful and take some actions.'
+   elif (pred>0.75):
+        pred='You have '+ str((1-pred)*100)+'%'+' possibility of having heart disease.You are safe'
  
     
    return {
