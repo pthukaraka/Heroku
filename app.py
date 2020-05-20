505f4f00e893b3fe596a5176f5949d15a5a814ed
pthukaraka <65617857+pthukaraka@users.noreply.github.com>
2020-05-20 12:36:41 +0530

    final

--- app.py
+++ app.py
@@ -15,55 +15,36 @@ global a
 a=[]
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    res = json.dumps(req['queryResult']['parameters'], indent=4)
-    int_features=json.loads(res)
-    a.append(int_features)
-    print(a)
-    
-    if(len(a)==13):
-         
-        r=get_data(a)
+        req = request.get_json(silent=True, force=True)
+        res = json.dumps(req['queryResult']['parameters'], indent=4)
+        int_features=json.loads(res)
+        print(int_features)
+        final_features=pd.DataFrame({'age':[int(int_features['age']['amount'])],'sex':[int(int_features['sex'])],'cp':[int(int_features['cp'])],'trestbps':[int(int_features['trestbps'])],'chol':[int(int_features['chol'])],'fbs':[int(int_features['fbs'])],'restecg':[int(int_features['restecg'])],'thalach':[int(int_features['thalach'])],'exang':[int(int_features['exang'])],'oldpeak':[float(int_features['oldpeak'])],'slope':[float(int_features['slope'])],'ca':[int(int_features['ca'])],'thal':[int(int_features['thal'])]})
+        prediction=model.predict_proba(final_features)
+        output=prediction[0][1]*100
+        r=get_data(output)
         print(r)
         r=json.dumps(r)
-        
         result = make_response(r)
-        print(result)
         result.headers['Content-Type'] = 'application/json'
         return result
-    else:
-        return ("hi")
-    
-     
-    
-     
-    #r = make_response(result)
-    #r.headers['Content-Type'] = 'application/json'
      
  
-def get_data(int_features):
-   dic={}
-   for d in int_features:
-        dic={**dic,**d} 
-   final_features=pd.DataFrame({'age':[int(dic['age'][0]['amount'])],'sex':[int(dic['sex'])],'cp':[int(dic['cp'])],'trestbps':[int(dic['trestbps'])],'chol':[int(dic['chol'])],'fbs':[int(dic['fbs'])],'restecg':[int(dic['restecg'])],'thalach':[int(dic['thalach'])],'exang':[int(dic['exang'])],'oldpeak':[float(dic['oldpeak'])],'slope':[float(dic['slope'])],'ca':[int(dic['ca'])],'thal':[int(dic['thal'])]})
-   prediction=model.predict_proba(final_features)
-   #output='{0:.{1}f}'.format(prediction[0][1],2)
-   output=prediction[0][1]*100
+def get_data(output):
    if output >=50:
         pred='You have '+ str(output)+'%'+' possibility of having heart disease.Please take immediate action to cure your self.'
    elif 30<=output<=50:
         pred='You have '+ str(output)+'%'+' possibility of having heart disease.Please take any actions and precautions to not to get a heart disease.'
-   elif output<=30:
+   elif 0<output<=30:
+        pred='You have '+ str(output)+'%'+' possibility of having heart disease.Anyway you have to be careful.'
+   elif 0==output:
         pred='You have '+ str(output)+'%'+' possibility of having heart disease.You are safe'
-     
+ 
     
    return {
        "fulfillmentText" : pred,
    }
  
-
- 
-     
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 80))
 
